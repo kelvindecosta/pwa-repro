8e49c224016e753581ba11ce96c8f8fc01fc08af
Kelvin DeCosta <decostakelvin@gmail.com>
2024-09-15 22:42:54 +0300

    feat: make minimal repro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,48 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+    isr: true,
+    edgeMiddleware: true,
+  }),
+  build: {
+    format: "file",
+  },
+  integrations: [
+    AstroPWA({
+      mode: import.meta.env.DEV ? "development" : "production",
+      strategies: "generateSW",
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg"],
+      pwaAssets: {
+        config: true,
+        overrideManifestIcons: true,
+      },
+      manifest: {
+        name: "PWA Repro",
+        short_name: "PWA Repro",
+        description: "This is a bug reproduction for Astro PWA",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{html,js,css,ico,png,svg,webp,woff,woff2}"],
+        cleanupOutdatedCaches: true,
+      },
+      devOptions: {
+        enabled: true,
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  trailingSlash: "never",
+});
---
+++ pwa-assets.config.ts
@@ -0,0 +1,20 @@
+/**
+ * Vite PWA Assets Generator configuration.
+ *
+ * References:
+ *
+ * - [Documentation](https://vite-pwa-org.netlify.app/assets-generator/)
+ */
+
+import {
+  defineConfig,
+  minimal2023Preset,
+} from "@vite-pwa/assets-generator/config";
+
+export default defineConfig({
+  headLinkOptions: {
+    preset: "2023",
+  },
+  preset: minimal2023Preset,
+  images: "public/favicon.svg",
+});
